5a910f5b674fc9e637a5375cda7abd70eea0f516
calesanz <8714917+calesanz@users.noreply.github.com>
2021-02-11 23:10:49 +0100

    Add check when rerunning same container

--- test/custom_init.sh
+++ test/custom_init.sh
@@ -2,14 +2,11 @@ set -e
 sudo microdnf install git
 sudo -u splunk  mkdir -p /opt/splunk/vcs/{git_tmp_backup,git_tmp_restore}
 sudo -u splunk  mkdir -p /opt/splunk/vcs/backup.git
-echo "dirs"
 sudo -u splunk sh -c "cd /opt/splunk/vcs/backup.git && git init --bare --shared"
-echo "init"
+if [ ! -d "/opt/splunk/vcs/backup.git" ]; then
 sudo -u splunk sh -c "cd /opt/splunk/vcs/ && git clone backup.git"
 sudo -u splunk sh -c "git config --global user.email 'backup@example.local'"
 sudo -u splunk sh -c "git config --global user.name 'BackupUser'"
-echo "clone"
 sudo -u splunk sh -c 'cd /opt/splunk/vcs/backup && touch test && git add test && git commit -a -m "init" && git push'
-echo "file"
- 
+fi
 /usr/sbin/entrypoint.sh start
