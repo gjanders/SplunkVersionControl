ac872be4ffd4fedf41a4ba937955dcc9d4b5b4a2
calesanz <8714917+calesanz@users.noreply.github.com>
2021-02-12 00:38:41 +0100

    Fix recreation check

--- test/custom_init.sh
+++ test/custom_init.sh
@@ -1,9 +1,9 @@
 set -e
 sudo microdnf install git
 sudo -u splunk  mkdir -p /opt/splunk/vcs/{git_tmp_backup,git_tmp_restore}
+if [ ! -d "/opt/splunk/vcs/backup.git" ]; then
 sudo -u splunk  mkdir -p /opt/splunk/vcs/backup.git
 sudo -u splunk sh -c "cd /opt/splunk/vcs/backup.git && git init --bare --shared"
-if [ ! -d "/opt/splunk/vcs/backup.git" ]; then
 sudo -u splunk sh -c "cd /opt/splunk/vcs/ && git clone backup.git"
 sudo -u splunk sh -c "git config --global user.email 'backup@example.local'"
 sudo -u splunk sh -c "git config --global user.name 'BackupUser'"
